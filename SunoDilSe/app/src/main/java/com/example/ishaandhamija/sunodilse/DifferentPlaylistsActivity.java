279bfda459cb32c6233cf00b572ab4e547bc3899
Ishaan Dhamija <ishaandhamija@Ishaans-MacBook-Air.local>
2017-04-21 02:07:10 +0530

    Runtime Permissions Added

--- SunoDilSe/app/src/main/java/com/example/ishaandhamija/sunodilse/DifferentPlaylistsActivity.java
+++ SunoDilSe/app/src/main/java/com/example/ishaandhamija/sunodilse/DifferentPlaylistsActivity.java
@@ -1,13 +1,20 @@
 package com.example.ishaandhamija.sunodilse;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.security.Permission;
+
 public class DifferentPlaylistsActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE = 3065;
     ImageView songs, artists, genres, albums;
 
     @Override
@@ -20,6 +27,15 @@ public class DifferentPlaylistsActivity extends AppCompatActivity {
         genres = (ImageView) findViewById(R.id.genres);
         albums = (ImageView) findViewById(R.id.albums);
 
+        if((ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE))== PackageManager.PERMISSION_DENIED && (ActivityCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE))==PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(DifferentPlaylistsActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE);
+        }
+        else{
+            activityStart();
+        }
+    }
+
+    private void activityStart() {
         songs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,4 +73,15 @@ public class DifferentPlaylistsActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==REQUEST_CODE){
+            if(grantResults[1]==PackageManager.PERMISSION_GRANTED){
+                activityStart();
+            }else{
+                finish();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 }
